38c599ad947043e2106b674afb1b84928e18485c
edu-fiap <84209899+edu-fiap@users.noreply.github.com>
2021-05-20 21:42:18 -0400

    Adicionando permissoes de execucao chmod

--- Checkpoint02/Intro1/CP02_Intro1.sh
+++ Checkpoint02/Intro1/CP02_Intro1.sh
@@ -66,7 +66,16 @@ function Question10(){
 #---------------------------------------------------
 
 
-
+Question1
+Question2
+Question3
+Question4
+Question5
+Question6
+Question7
+Question8
+Question9
+Question10
 
 
 
